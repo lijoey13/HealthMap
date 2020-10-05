1d27f2d223a8b2c8718920c12c079b3ca85fb776
dchung21 <dchung679@gmail.com>
2020-10-04 20:16:57 -0700

    tweaks

--- database.js
+++ database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 module.exports = {
 	pool: mysql.createPool({
 		connectionLimit: 10,
-		host: "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-		user: "sntzr8hroxqyfr0j",
-		password: "u77rq4qkpa361vcy",
-		database: "smktyb6nnz6aeuho",
+		host: "localhost",
+		user: "admin",
+		password: "password",
+		database: "clinics",
 		multipleStatements: true
 	})
 
